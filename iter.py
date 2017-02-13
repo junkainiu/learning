f7cfdc9887e35b3c30622482426b30aeda51e52a
leo.niu <junkainiu@gmail.com>
2017-02-13 22:41:05 +0800

    fix

--- iter.py
+++ iter.py
@@ -3,13 +3,12 @@ class IterRator(object):
     def __init__(self, l, n):
         self.l = l
         self.n = n
-        self.i = 0
 
     def __iter__(self):
-        for i in range(len(self.l)):
+        for i in range(0, len(self.l), 2):
             yield self.l[i: i+self.n]
 
 
-it = IterRator([1,2,3], 2)
+it = IterRator([1,2,3,4,5], 2)
 for item in it:
     print item
