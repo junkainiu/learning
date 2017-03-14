f3350e1bd0b651344e6e252d3c156120b959a1ef
leo.niu <leo.niu@netis.com.cn>
2017-03-14 18:04:08 +0800

    update

--- ds/week2ex3.py
+++ ds/week2ex3.py
@@ -1,40 +1,61 @@
 start, num, reverse_num = raw_input().split(' ')
 reverse_num = int(reverse_num)
+
+from collections import deque
+
+def reverse(l, last_addr='-1'):
+    for i in range(reverse_num - 1, -1, -1):
+        start, data, _next = l[i]
+        if i > 0:
+            print start, data, l[i-1][0]
+        else:
+            print start, data, last_addr
+
 lines = {}
-res = []
+tmp = deque()
+next_tmp = deque()
+
 for i in range(int(num)):
     node = raw_input().split(' ')
     lines[node[0]] = (node[1], node[2])
-data, addr = lines.pop(start)
-tmp = [(start, data ,addr)]
-while lines:
+
+data, addr = lines[start]
+tmp.append((start, data, addr))
+
+for i in range(min(num, reverse_num - 1)):
+    data, next_addr = lines[addr]
+    tmp.append((addr, data, next_addr))
+    addr = next_addr
+
+while True:
+    if addr == '-1':
+        break
+
     for i in range(reverse_num):
-        if len(tmp) == reverse_num:
-            break
         if addr == '-1':
             break
-        data, next_addr = lines.pop(addr)
-        tmp.append((addr, data, next_addr))
+        data, next_addr = lines[addr]
+        next_tmp.append((addr, data, next_addr))
         addr = next_addr
     if len(tmp) == reverse_num:
-        res.append(list(reversed(tmp)))
-    else:
-        res.append(tmp)
-    tmp = []
-if not res:
-    res.append(tmp)
-for index, sub_res in enumerate(res):
-    if len(sub_res) == reverse_num:
-        for i, item in enumerate(sub_res):
-            start, data, _next = item
-            if i < len(sub_res) - 1:
-                sub_res[i] = (start, data, sub_res[i+1][0])
-            else:
-                if index == len(res) - 1:
-                    sub_res[i] = (start, data, '-1')
-                else:
-                    sub_res[i] = (start, data, res[index+1][0][0])
+        if len(next_tmp) == reverse_num:
+            reverse(tmp, last_addr=next_tmp[-1][0])
+            tmp, next_tmp = next_tmp, deque()
+        else:
+            if next_tmp:
+                reverse(tmp, last_addr=next_tmp[0][0])
+                tmp, next_tmp = next_tmp, deque()
 
-for sub_res in res:
-    for i in sub_res:
-        print ' '.join(list(i))
+if tmp:
+    if len(tmp) == reverse_num:
+        reverse(tmp)
+    else:
+        for i in tmp:
+            print ' '.join(i)
+
+if next_tmp:
+    if len(next_tmp) == reverse_num:
+        reverse(next_tmp)
+    else:
+        for i in next_tmp:
+            print ' '.join(i)
